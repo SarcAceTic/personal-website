123f3d1131b089a33f5cf2c47c3ab72046489b6d
SarcAceTic <sarcacetic@gmail.com>
2024-02-26 22:10:54 +0700

    Added layouts

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <AppHeader />
+        <slot />
+    </div>
+</template>
+
+<script setup>
+import AppHeader from '../components/AppHeader.vue';
+
+
+</script>
+
+<style>
+
+</style>
