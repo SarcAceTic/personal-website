93460630d787d15f2cf17c83157cba3e8b5fb456
SarcAceTic <sarcacetic@gmail.com>
2024-03-02 21:25:50 +0700

    Added about.vue page

---
+++ pages/about.vue
@@ -0,0 +1,30 @@
+<template>
+ <div class="pt-24">
+  <section class="mx-6">
+   <h2 class="font-pops text-3xl text-center uppercase">About website</h2>
+   <Divider class="my-4" />
+   <h3 class="font-pops text-2xl uppercase my-4 mb-6">What is this website?</h3>
+   <p>
+    This website is actually an experimental one, making myself familiar with
+    new libraries and frameworks, but it's also a
+    silly goofy little project that I made as some sort of Portfolio and self
+    introduction, where I can show off my work and other projects aswell.
+   </p>
+   <h3 class="font-pops text-2xl uppercase my-6">How did you make this?</h3>
+   <p>
+    The UI and UX components was made with <NuxtLink class="text-purple-700" target="_blank" to="https://www.primevue.org">PrimeVue</NuxtLink> and the customization and
+    styling was assisted / made with <NuxtLink class="text-purple-700" target="_blank" to="https://www.tailwindcss.com">Tailwind CSS</NuxtLink>. The framework I'm using is
+    <NuxtLink class="text-purple-700" target="_blank" to="https://www.vuejs.org">Vue</NuxtLink>, and <NuxtLink class="text-purple-700" target="_blank" to="https://www.nuxt.com">Nuxt</NuxtLink>.
+   </p>
+   <Divider class="my-6" />
+   <h3 class="font-pops text-2xl text-center uppercase my-6">Author's Note</h3>
+   <p class="italic">Constant updates and changes are going to be made on this website, so stay tuned <span class="text-pink-600 pi pi-heart-fill"></span></p>
+  </section>
+ </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
  <div class="flex-1 p-4">
   <WhoIAm class="my-4 mb-8" />
+  <Projects class="my-4 mb-8" />
   <FAQSection class="my-4" />
  </div>
 </template>
