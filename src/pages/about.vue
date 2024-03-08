cf36742dbb035106a758ab147d4cfa37b40ea3bf
SarcAceTic <sarcacetic@gmail.com>
2024-03-08 19:39:56 +0700

    Added blog.vue page

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -6,25 +6,50 @@
    <h3 class="font-pops text-2xl uppercase my-4 mb-6">What is this website?</h3>
    <p>
     This website is actually an experimental one, making myself familiar with
-    new libraries and frameworks, but it's also a
-    silly goofy little project that I made as some sort of Portfolio and self
-    introduction, where I can show off my work and other projects aswell.
+    new libraries and frameworks, but it's also a silly goofy little project
+    that I made as some sort of Portfolio and self introduction, where I can
+    show off my work and other projects aswell.
    </p>
    <h3 class="font-pops text-2xl uppercase my-6">How did you make this?</h3>
    <p>
-    The UI and UX components was made with <NuxtLink class="text-purple-700" target="_blank" to="https://www.primevue.org">PrimeVue</NuxtLink> and the customization and
-    styling was assisted / made with <NuxtLink class="text-purple-700" target="_blank" to="https://www.tailwindcss.com">Tailwind CSS</NuxtLink>. The framework I'm using is
-    <NuxtLink class="text-purple-700" target="_blank" to="https://www.vuejs.org">Vue</NuxtLink>, and <NuxtLink class="text-purple-700" target="_blank" to="https://www.nuxt.com">Nuxt</NuxtLink>.
+    The UI and UX components was made with
+    <NuxtLink
+     class="text-purple-700"
+     target="_blank"
+     to="https://www.primevue.org">
+     PrimeVue
+    </NuxtLink>
+    and the customization and styling was assisted / made with
+    <NuxtLink
+     class="text-purple-700"
+     target="_blank"
+     to="https://www.tailwindcss.com">
+     Tailwind CSS
+    </NuxtLink>
+    . The framework I'm using is
+    <NuxtLink
+     class="text-purple-700"
+     target="_blank"
+     to="https://www.vuejs.org">
+     Vue
+    </NuxtLink>
+    , and
+    <NuxtLink class="text-purple-700" target="_blank" to="https://www.nuxt.com">
+     Nuxt
+    </NuxtLink>
+    .
    </p>
    <Divider class="my-6" />
    <h3 class="font-pops text-2xl text-center uppercase my-6">Author's Note</h3>
-   <p class="italic">Constant updates and changes are going to be made on this website, so stay tuned <span class="text-pink-600 pi pi-heart-fill"></span></p>
+   <p class="italic">
+    Constant updates and changes are going to be made on this website, so stay
+    tuned
+    <span class="text-pink-600 pi pi-heart-fill"></span>
+   </p>
   </section>
  </div>
 </template>
 
 <script setup></script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/pages/blog.vue
@@ -0,0 +1,24 @@
+<template>
+ <div class="pt-24 text-center">
+  <section class="mx-6">
+   <div class="flex justify-center mb-4 mt-10">
+    <Image class="w-32 h-auto" src="/MarchThumbs.png" />
+   </div>
+   <h2 class="font-pops text-3xl uppercase">Hang in there</h2>
+   <Divider class="my-4" />
+   <p class="my-4">
+    Hey! You tried accessing my
+    <b>blog</b>
+    ... and well... unfortunately, it's still in the works! So... hang in there,
+    and hopefully I'll finish it ASAP!
+   </p>
+   <NuxtLink to="/">
+    <Button class="!focus:ring-0" label="Take me back home!" link />
+   </NuxtLink>
+  </section>
+ </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
